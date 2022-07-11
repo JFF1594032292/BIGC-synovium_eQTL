a9e863c52a85b530b147bd9fa8c55ba53adc5e93
JFF <1594032292@qq.com>
2022-07-11 16:11:35 +0800

    test

---
+++ 2-Gene_expression/1-test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#########################################################################
+# File Name: 1-test.py
+# Created on : 2022-07-11 16:11:11
+# Author: JFF
+# Last Modified: 2022-07-11 16:11:13
+# Description:
+# Usage:
+# Input:
+# Output:
+#########################################################################
+import os
+import sys
+import re
+import time
+import math
+import random
